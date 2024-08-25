9ca6e3f904635dd7d01f03a891a371fa62d49ad1
hn4teiv <22010431@st.phenikaa-uni.edu.vn>
2024-08-25 10:40:48 +0700

    code2

---
+++ tuan1/ForExample.java
@@ -0,0 +1,13 @@
+/*Use a for loop to print "Yes" 5 times. */
+
+public class ForExample {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Yes");
+        }
+        
+    }
+
+    
+    
+}
---
+++ tuan1/Switch.java
@@ -0,0 +1,15 @@
+public class Switch {
+    public static void main(String[] args) {
+        int day = 2;
+        switch (day) {
+           case 1:
+            System.out.println("Saturday");
+            break;
+           case 2:
+            System.out.println("Sunday");
+            ;
+        }
+        
+    }
+    
+}
